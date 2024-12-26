0cbfcbfa4e7f4af3e6e91336419aa9b16dfa2569
Yjcq <yjcq007@gmail.com>
2024-12-26 18:56:12 -0400

    add swagger instalacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,15 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MetropolisModule } from './metropolis/metropolis.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal:true }), //Esto configura para que las variables de entorno esten disponibles para cualquier parte del codigo
     AuthModule,
-    MetropolisModule],
+    MetropolisModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+config();
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('v1');
+
+  const config = new DocumentBuilder()
+  .setTitle('metropolis-api')
+  .setDescription('')
+  .setVersion('1.0')
+  .addTag('api')
+  .build();
+  
+  const documentFactory = () => SwaggerModule.createDocument(app,  config);
+  SwaggerModule.setup('api', app, documentFactory)
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
