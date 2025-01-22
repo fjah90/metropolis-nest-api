7e3717000c5ad13389a83f9b9ee0a493ef2c9c16
Fernando Aponte <fernandoaponte90@gmail.com>
2025-01-21 23:06:05 -0400

    feat: integrate Bill and BillStorage modules into the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,33 +5,31 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { PrinterModule } from './metropolis/printer/printer.module';
-import { PdfSigningService } from './metropolis/pdf-signing/pdf-signing.service';
-import { PdfSigningController } from './metropolis/pdf-signing/pdf-signing.controller';
-import { FacturaModule } from './metropolis/factura/factura.module';
+import { BillModule } from './metropolis/bill/bill.module';
 import { MulterModule } from '@nestjs/platform-express';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PdfSigningModule } from './metropolis/pdf-signing/pdf-signing.module';
-
+import { BillStorageModule } from './metropolis/bill-storage/bill-storage.module';
 
 @Module({
   imports: [
     MulterModule.register({
       dest: './uploads', // Directorio donde se guardarán los archivos temporalmente
-      }),
-    ConfigModule.forRoot({ isGlobal:true }), //Esto configura para que las variables de entorno esten disponibles para cualquier parte del codigo
+    }),
+    ConfigModule.forRoot({ isGlobal: true }), //Esto configura para que las variables de entorno esten disponibles para cualquier parte del codigo
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
       serveRoot: '/public',
-     }), // Opcional: para acceder a través de '/public/output' }),
+    }), // Opcional: para acceder a través de '/public/output' }),
     AuthModule,
     UsersModule,
     PrinterModule,
-    FacturaModule,
-    PdfSigningModule
-
+    PdfSigningModule,
+    BillModule,
+    BillStorageModule,
   ],
-  controllers: [AppController, PdfSigningController],
-  providers: [AppService, PdfSigningService],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
